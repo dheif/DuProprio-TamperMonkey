350d9d8397b44cf918dd069132cafd7a8ae586cd
Sebastien Giroux <sgiroux@gmail.com>
2017-05-30 07:47:59 -0400

    Update duproprio.user.js

--- duproprio.user.js
+++ duproprio.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         duproprio
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://duproprio.com/*
@@ -10,12 +10,22 @@
 
 (function() {
     'use strict';
-    addGlobalStyle('.search-results-container__featured-properties {display: none !important; }');
+    addGlobalStyle('.search-results-feature-properties {display: none !important; }');
+    addGlobalStyle('.search-results-listings-container__main-content {max-width: none !important; }');    
+    
     addGlobalStyle('.search-results-listings-third-column {display: none !important; }');
     addGlobalStyle('.search-results-listings-form__alert-email {display: none !important; }');
-    addGlobalStyle('.search-results-listings-list__item {max-width: 20% !important; }');
+    addGlobalStyle('.search-results-listings-list__item {max-width: 23% !important; }');  
+        
     addGlobalStyle('.photo-viewer__slide {max-width: 400px !important; }');
-    addGlobalStyle('.photo-viewer__slide.slick-current {max-width: 408px !important; }');
+    addGlobalStyle('.photo-viewer__slide.slick-current {max-width: 408px !important; }'); 
+    addGlobalStyle('.photo-viewer__slide::after {background: none !important; }');    
+    
+    addGlobalStyle('.listing-costs-mortgage {display: none !important; }');    
+    addGlobalStyle('.featured-company {display: none !important; }');    
+   
+    addGlobalStyle('.slick-slide {border-left:  4px solid #f6f6f6 !important; }');
+    addGlobalStyle('.slick-slide {border-right:  4px solid #f6f6f6 !important; }');
     
     
 })();
